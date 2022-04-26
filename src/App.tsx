8663a9ebbd5bb7ce61173557830e8eb0489726bd
felipehw <felipehw@gmail.com>
2022-04-26 17:55:57 -0300

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,30 @@ import memories from './images/memories.png';
 import Form from './components/Form/form';
 import Posts from './components/Posts/posts';
 
-const MemoriesImg = styled.img`
+const StyledAppBar = styled(AppBar)`
+  border-radius: 15px;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledTypography = styled(Typography)`
+  color: rgba(0,183,255,1);
+`;
+const StyledImg = styled.img`
   height: 60px;
+  margin-left: 15px;
   width: 60px;
 `;
 
 const App = () => {
   return (
     <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <MemoriesImg src={memories} alt='memories' />
-      </AppBar>
+      <StyledAppBar position='static' color='inherit'>
+        <StyledTypography variant='h2' align='center'>Memories</StyledTypography>
+        <StyledImg src={memories} alt='memories' />
+      </StyledAppBar>
       <Grow in>
         <Container>
           <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
--- src/components/Posts/posts.tsx
+++ src/components/Posts/posts.tsx
@@ -12,3 +12,23 @@ const Posts = () => {
 };
 
 export default Posts;
+
+/*
+// Posts styles
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
+
+*/
