d9547dd377d504d91623bc938c8d1abeb69b0c74
felipehw <felipehw@gmail.com>
2022-04-26 17:21:10 -0300

    JSX Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import styled from '@emotion/styled';
+
+import memories from './images/memories.png';
+import Form from './components/Form/form';
+import Posts from './components/Posts/posts';
+
+const MemoriesImg = styled.img`
+  height: 60px;
+  width: 60px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <MemoriesImg src={memories} alt='memories' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
